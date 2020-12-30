94fe61b5a91fef440169f26c9ea2e4670a383087
msyesyan <msyesyan@gmail.com>
2020-12-30 15:39:52 +0800

    remove useless message

--- src/extension.ts
+++ src/extension.ts
@@ -18,8 +18,6 @@ export function activate(context: vscode.ExtensionContext) {
       // The code you place here will be executed every time your command is executed
 
       // Display a message box to the user
-			vscode.window.showInformationMessage('Hello World from json-sorter!');
-
 			const editor = vscode.window.activeTextEditor;
       if (!editor) {
         return;
